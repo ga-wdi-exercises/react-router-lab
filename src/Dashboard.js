f3306789b22699eb32efa50754a35165e8496be9
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-03-28 13:49:35 -0400

    dashboard displays stocks

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react'
-import { link } from "react-router-dom"
-import App from './App'
+
 
 class Dashboard extends Component {
   constructor(props){
     super(props)
-    console.log(props)
+
   }
 
   render(){
-    let stocks = this.props.stocks
+
+    let stocks = this.props.stocks.map((stock, index) => {
+      return <div>{stock.name} | {stock.symbol}</div>
+    })
 
     return(
       <div>
         <h1>Stocks</h1>
-        {stocks.forEach(stock, i){
-          return({stock.name})
-        }}
+        {stocks}
     </div>
     )
     }
