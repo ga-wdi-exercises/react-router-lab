33ad77067c6c001d9aa8e6ff3782593575a6c85d
Bedig Galladian <bgalladian@gmail.com>
2017-03-28 11:56:12 -0400

    got about page link working

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return(
+      <div>
+        <p> This the about page</p>
+      </div>
+    )
+  }
+}
+
+export default About
---
+++ src/Stocks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Stock extends Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return(
+    )
+  }
+}
+
+export default Stock
