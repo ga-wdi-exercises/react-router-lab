69f5f499fd9f2bbf0fae2b02c41d0019715dc289
Taylor Scott <TaylorScott@Taylors-MacBook-Pro.local>
2017-03-28 12:46:09 -0400

    part one of lab

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h2>This is an app to track stocks, made in React. An API will be attached one day, maybe!</h2>
+      </div>
+    );
+  }
+}
+
+export default About;
