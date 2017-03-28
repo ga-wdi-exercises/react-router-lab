4fecde18ec54c58e17db289c04e168163262d054
Samuel Clements <sdkclements@gmail.com>
2017-03-28 09:52:34 -0400

    added basic nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Redirect
+} from 'react-router-dom'
+
 
 class App extends Component {
+
+
+
   render() {
     return (
-      // <Router> ... </Router>
+        <Router>
+            <div>
+                <nav>
+                    Tick my Stock {'\u00A0'}
+                    <Link to="/home"> Home </Link> {'\u00A0'} | {'\u00A0'}
+                    <Link to="/about"> About </Link>
+                </nav>
+            </div>
+        </Router>
+
     );
   }
 }
