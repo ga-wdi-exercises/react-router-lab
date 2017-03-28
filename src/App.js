514885fef6c98eec6906069cd2f0f8ecaebb7c14
Habtamu Abere <habbuya@gmail.com>
2017-03-28 10:53:39 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -12,13 +12,48 @@ class App extends Component {
     return (
       <Router>
       <div>
-      
+
       <div className="nav">
         <div className="nav-item"><span className="nav-logo">iStocks</span></div>
         <div className="nav-item"><Link to="/">Home</Link></div>
         <div className="nav-item"><Link to="/search">Search</Link></div>
         <div className="nav-item"><Link to="/about">About</Link></div>
       </div>
+      <main>
+         <Route
+         path="/search"
+         render={() =>{
+           return(
+
+               <h2>Hello</h2>
+
+           )
+         }  }
+         />
+
+         <Route
+      path="/about"
+      render={() =>{
+        return(
+           <h2>About</h2>
+
+        )
+      }  }
+      />
+
+      <Route
+      path="/"
+      render={() => {
+        return <h2>Home page</h2>
+      }}
+      />
+      <Route
+      path="/*"
+      render={() => {
+        return <Redirect to="/" />
+      }}
+      />
+      </main>
 
 
 
