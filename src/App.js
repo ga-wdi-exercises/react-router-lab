49aa1c80034e7f5663e29df0bd4a5ac183d2b840
Eva Lai <evalai.dc@gmail.com>
2017-03-29 10:23:38 -0400

    added navigation bar to linkto different urls

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div></div>
+        <div className="nav">
+          <div className="nav-item"><span className="nav-logo">iStocks</span></div>
+          <div className="nav-item"><Link to="/">Home</Link></div>
+          <div className="nav-item"><Link to="/search">Search</Link></div>
+          <div className="nav-item"><Link to="/about">About</Link></div>
+          </div>
       </Router>
     );
   }
