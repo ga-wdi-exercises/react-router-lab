1d6b9bea544229cc77eaf21d3c1da5b301ae3510
KEVIN KELLER <keller_kevin@bah.com>
2017-03-29 12:14:47 -0400

    content on about page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Stock from './Stock';
+import About from './About';
+import Dashboard from './Dashboard';
 import './index.css';
 
 ReactDOM.render(
