0e93057a3989c58fc941d813f19d51e4a2385541
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-03-29 12:21:18 -0400

    fixed spacing and indentation

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,12 @@ class About extends Component {
   render(){
     return (
       <div>
-    <h1>iStocks</h1>
-    <p>
-    Welcome to iStocks. This is an app created with ReactJS that tracks stock market information for users. Enjoy!
-    </p>
+        <h1>iStocks</h1>
+        <p>
+        Welcome to iStocks. This is an app created with ReactJS that tracks stock market information for users. Enjoy!
+        </p>
       </div>
-  )
-}
+    )
+  }
 }
 export default About;
