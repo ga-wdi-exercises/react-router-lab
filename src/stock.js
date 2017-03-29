62f3ffa8293d94c587264fefa3c6ba8490c378e7
Travis Batts <Travisbatts6@gmail.com>
2017-03-29 13:38:34 -0400

    save

---
+++ src/stock.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+ import { Link } from "react-router-dom"
+ 
+ class Stock extends Component {
+   constructor(props) {
+     super(props)
+   }
+   render(){
+     return(
+ 
+       <div>
+         <h3>{this.props.stock.name} - <Link to={{
+                                         pathname: `stocks/${this.props.stock.symbol}`,
+                                         state: {stockPick: this.props.stock}
+                                         }}
+                                       >{this.props.stock.symbol}</Link></h3>
+      </div>
+     )
+   }
+ }
+ 
+ +export default Stock
