8fcbdf15376086c4ece7259c0712b51ec2560114
Gurpreet Gill <gill_gurpreet2@bah.com>
2017-03-29 11:47:54 -0400

    init

--- src/App.js
+++ src/App.js
@@ -5,13 +5,32 @@ import {
   Link,
   Redirect
 } from "react-router-dom"
+import Dashboard from "./Dashboard"
+import Stock from "./Stock"
+import About from "./About"
 import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      stocks: [
+        {name: "Booz", symbol: "BAH", lastPrice: 35.63, change: -0.19, high: 35.88, low: 35.58, open: 35.77}
+      ]
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div></div>
+        <div>
+         <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+         </nav>
+        </div>
       </Router>
     );
   }
---
+++ src/Dashboard.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom"
+
+class Dashboard extends Componennt {
+
+}
+
+export default Dashboard;
