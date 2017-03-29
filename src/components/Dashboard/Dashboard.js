ef87604e6c4c4ad0618503138724c59940b6f533
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-29 12:06:41 -0400

    dashboard completed

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,25 @@
+import React, { Component} from 'react'
+import './Dashboard.css'
+
+class Dashboard extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    let stocks = this.props.stocks.map((stock, index) =>{
+      return(
+        <li key={index}>{stock.name} {stock.symbol}</li>
+      )
+    })
+    return(
+      <div className="stocks">
+        <h2>Stocks</h2>
+      <ul className="stocks-list">
+       {stocks}
+      </ul>
+    </div>
+    )
+  }
+}
+
+export default Dashboard
