e78b9d171f55a47e9a2b61edfea288bd674de68e
shapiro_benjamin@bah.com <benjaminshapiro@C1MS50ZQH3QK.local>
2017-03-29 17:02:37 -0400

    Complete Part 1 w/ style

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class About extends Component{
+  render(){
+    return(
+      <h1>Hello this is my stock web site I hope you like it</h1>
+    )
+  }
+}
+
+export default About
