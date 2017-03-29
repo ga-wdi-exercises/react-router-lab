20779d745a151579e1542366c9e3fe05160d7683
Collin OConnell <ococncol@gmail.com>
2017-03-29 11:15:55 -0400

    got navbar setup

--- src/App.js
+++ src/App.js
@@ -11,7 +11,19 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div></div>
+        <div>
+          <div className="nav">
+            <div className="nav-item"><span className="nav-logo">iStocks</span></div>
+            <div className="nav-item"><Link to="/">Home</Link></div>
+            <div className="nav-item"><Link to="/about">About</Link></div>
+          </div>
+
+          <div className="main">
+            <Route exact path="/" render={() => <Dashboard stocks={this.state.stocks} />} />
+            <Route path="/about" component={About} />
+            <Route path="/stocks/:symbol" component={Stock} />
+          </div>
+        </div>
       </Router>
     );
   }
