7f2dd26be262eddd75681aefb8e867803d2d9ab5
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-05-30 18:13:09 -0400

    part 2 almost

--- src/App.js
+++ src/App.js
@@ -1,61 +1,80 @@
 import React, { Component } from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Redirect
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Redirect
 } from "react-router-dom"
 import './App.css';
+import axios from "axios"
 import Dashboard from './Dashboard.js'
 import Stock from "./Stock.js"
 import About from "./About.js"
+import Search from "./Search.js"
 
 
 class App extends Component {
- constructor(props){
-  super(props)
-  this.state = {
-    stocks: [
-      {name: "Apple Inc.", symbol: "AAPL", lastPrice: 140.64, change: -0.280000000000001, high: 141.74, low: 140.35, open: 141.5},
-      {name: "Microsoft Corporation", symbol: "MSFT", lastPrice: 64.98, change: 0.109999999999999, high: 65.45, low: 64.76, open: 65.12},
-      {name: "Alphabet Inc.", symbol: "GOOGL", lastPrice: 835.14, change: -4.50999999999999, high: 844, low: 829.1, open: 842},
-      {name: "Facebook, Inc.", symbol: "FB", lastPrice: 140.34, change: 0.810000000000002, high: 141.0244, low: 139.76, open: 140.08},
-      {name: "Oracle Corporation", symbol: "ORCL", lastPrice: 44.65, change: -0.300000000000004, high: 45.09, low: 44.575, open: 44.91},
-      {name: "Intel Corporation", symbol: "INTL", lastPrice: 36.16, change: -0.370000000000005, high: 36.78, low: 36.125, open: 36.58}
-    ]
-  }
-}
-  render() {
-    return (
-      <Router>
-        <div>
-        <nav>
-          <h2>Stock Data</h2>
-          <div>
+    constructor(props){
+        super(props)
+        this.state = {
+            stocks: [],
+            hasSearched: true
+        }
+        this.handleSearchedState = this.handleSearchedState.bind(this)
+    }
+    componentDidMount(){
+        axios.get("http://localhost:3000/stocks").then((response) => {
+            this.setState({
+                stocks: response.data
+            })
+        })
+    }
+    handleSearchedState(newStock){
+        let tempArray = this.state.stocks
+        tempArray.push(newStock)
+        this.setState({
+            stocks: tempArray,
+            hasSearched: true
+        })
+    }
+    render() {
+        return (
+            <Router>
+            <div>
+            <nav>
+            <h2>Stock Data</h2>
+            <div>
             <Link to="/">Home</Link>
             <Link to="/About">About</Link>
-          </div>
-        </nav>
+            <Link to="/search">Search</Link>
+            </div>
+            </nav>
 
-        <main>
-        <Route
-        exact path="/"
-        render={() => {
-            return (
-                <Dashboard stocks={this.state.stocks} />
-            )
-        }}
-        />
+            <main>
+            <Route
+            exact path="/"
+            render={() => {
+                return (
+                    <Dashboard stocks={this.state.stocks} />
+                )
+            }}
+            />
 
-        <Route
-        path="/stocks/:symbol" component={Stock} />
-        <Route
-        path="/about" component={About} />
-        </main>
-        </div>
-      </Router>
-    );
-  }
+            <Route
+            path="/stocks/:symbol" component={Stock} />
+            <Route
+            path="/about" component={About} />
+            <Route path="/search" render={() => {
+              if(this.state.hasSearched){
+                return <Redirect to="/" />
+              }
+              return <Search handleSearchedState={this.handleSearchedState} />
+            }} />
+            </main>
+            </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- src/Stock.js
+++ src/Stock.js
@@ -1,22 +1,33 @@
-
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Stock extends Component {
   constructor(props){
     super(props)
     this.state = {
-      stock: this.props.location.state.selectedStock
+      stock: this.props.location.state.selectedStock,
+      apiStock: {}
     }
   }
+
+  componentDidMount(){
+  axios.get ("http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=" + this.state.selectedStock.symbol)
+  .then((response) => {
+     this.setState({
+        apiStock: response.data
+     })
+  })
+}
+
   render() {
     return (
       <div>
-        <h2>{this.state.stock.name} ({this.state.stock.symbol})</h2>
+        <h2>{this.state.apiStock.name} ({this.state.apiStock.symbol})</h2>
         <ul>
-          <li>Current Price: {this.state.stock.lastPrice}</li>
-          <li>Change: {this.state.stock.change}</li>
-          <li>High: {this.state.stock.high}</li>
-          <li>Low: {this.state.stock.low}</li>
+          <li>Current Price: {this.state.apiStock.lastPrice}</li>
+          <li>Change: {this.state.apiStock.change}</li>
+          <li>High: {this.state.apiStock.high}</li>
+          <li>Low: {this.state.apiStock.low}</li>
         </ul>
       </div>
     );
