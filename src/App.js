39821d066536472c74159a33942acfd122c78118
Andres Arango <andresarangov17@gmail.com>
2017-05-30 10:53:41 -0400

    navigation bar. links to home and about

--- src/App.js
+++ src/App.js
@@ -7,11 +7,34 @@ import {
 } from "react-router-dom"
 import './App.css';
 
+class Home extends Component {
+  render() {
+    return(
+      <div>Home</div>
+    )
+  }
+}
+
+class About extends Component {
+  render() {
+    return(
+      <div>About</div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <Router>
-        <div></div>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </div>
       </Router>
     );
   }
