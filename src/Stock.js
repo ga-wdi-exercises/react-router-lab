d9b2e403044ad5aaf83cfa503e0cdcbf0f9d2e4d
Jonathan Wright <jonwright.va@gmail.com>
2017-05-30 14:26:19 -0400

    Part 1 complete

--- src/Stock.js
+++ src/Stock.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
+import stockData from '../data/stock-data.json'
 
 class Stock extends Component {
 	render() {
-		let stock = this.props.match.params.symbol
+		let stockSymbol = this.props.match.params.symbol
+		let stock = stockData.map((item, index) => {
+			if (item.symbol === stockSymbol) {
+				return (
+					<div key={index}>
+						<h2>{item.name} ({item.symbol})</h2>
+						<p></p>
+						<p>High: {item.high}</p>
+						<p>Low: {item.low}</p>
+						<p>Open: {item.open}</p>
+					</div>
+				)
+			}
+		})
 		return (
 			<div>
 				<h3>{stock}</h3>	
