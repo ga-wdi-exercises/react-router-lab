5a684547709765885f4cda990c446d2a36220e72
Caroline Chen <carolinecaroline@gmail.com>
2017-05-30 11:32:32 -0400

    create links, test mapping out stock data

--- src/App.js
+++ src/App.js
@@ -6,12 +6,31 @@ import {
   Redirect
 } from "react-router-dom"
 import './App.css';
+import stockData from "../data/stock-data"
+
+
 
 class App extends Component {
   render() {
+    let stocks = stockData.map((stock, index) => {
+      return (
+        <div key={index}>
+          <p>{stock.name}</p>
+          <p>{stock.symbol}</p>
+          <p>{stock.lastPrice}</p>
+        </div>
+      )
+    })
     return (
       <Router>
-        <div></div>
+        <div>
+        <p>Navbar</p>
+          <nav>
+              <div><Link to="/home">Home</Link></div>
+              <div><Link to="/search">Search</Link></div>
+           </nav>
+           {stocks}
+        </div>
       </Router>
     );
   }
