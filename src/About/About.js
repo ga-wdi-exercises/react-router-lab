3cb770f822bc2bf14794cdeda005d5d9d780e1d3
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-05-30 11:30:13 -0400

    finish part 1

--- src/About/About.js
+++ src/About/About.js
@@ -1,1 +1,17 @@
 import React, { Component } from 'react'
+
+class About extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render(){
+    return (
+      <div>
+        <h1> About </h1>
+        <h2> Stock Tracking Application </h2>
+      </div>
+    )
+  }
+}
+
+export default About;
