76c3af0af8564b7342a55f315b87ac29d7683af1
Whitney <whitney.purdum@gmail.com>
2017-05-30 11:21:51 -0400

    set up routes

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h2>About will be here.</h2>
+      </div>
+    );
+  }
+}
+
+
+export default About;
---
+++ src/Stock.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+class About extends Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h2>Stocks will be here.</h2>
+      </div>
+    );
+  }
+}
+
+
+export default Stock;
