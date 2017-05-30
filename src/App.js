38192ad34e00e7ab80ec2629bfc1dd49150d0bbd
Jonathan Wright <jonwright.va@gmail.com>
2017-05-30 11:16:10 -0400

    adds routes, redirect, about and stocks components

--- src/App.js
+++ src/App.js
@@ -6,12 +6,33 @@ import {
   Redirect
 } from "react-router-dom"
 import './App.css';
+import About from './About.js'
+import Stocks from './Stocks.js'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div></div>
+        <div>
+          <nav className="nav">
+            <Link to="/stocks" className="nav-logo">StockStuff</Link>
+            <Link to="/stocks">Stocks</Link>
+            <Link to="/about">About</Link>
+          </nav>
+        <Route path="/" render={() => {
+          return <Redirect to="/stocks" />
+        }} />
+        <Route path="/stocks" render={() => {
+          return (
+            <Stocks />
+          )
+        }} />
+        <Route path="/about" render={() => {
+          return (
+            <About />
+          )
+        }} />
+        </div>
       </Router>
     );
   }
---
+++ src/Stocks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Stocks extends Component {
+	render() {
+		return (
+			<div>
+				<h3>This is the Stocks Page</h3>
+			</div>
+		)
+	}
+}
+
+export default Stocks
