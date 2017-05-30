137ff0424fcb2dd3a08952b2d9a9ee674cfd586b
Whitney <whitney.purdum@gmail.com>
2017-05-30 18:11:10 -0400

    update

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom"
 import "./Dashboard.css"
+import axios from "axios"
 
 class Dashboard extends Component {
   constructor(props){
