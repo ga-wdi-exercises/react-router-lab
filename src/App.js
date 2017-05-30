f509fab0139f808b166efdfc022a57d95c27c221
Paul Christian-Brown <pchristianbrown@gmail.com>
2017-05-30 12:23:47 -0400

    Initial Commits

--- src/App.js
+++ src/App.js
@@ -6,8 +6,49 @@ import {
   Redirect
 } from "react-router-dom"
 import './App.css';
+import Dashboard from './Dashboard'
+import Stock from './Stock'
+import About from './About'
+import Index from './Index'
+import Search from './Search'
 
-class App extends Component {
+class Dashboard extends Component {
+  render() {
+    return (
+      <Router>
+        <div></div>
+      </Router>
+    );
+  }
+}
+class Stock extends Component {
+  render() {
+    return (
+      <Router>
+        <div></div>
+      </Router>
+    );
+  }
+}
+class About extends Component {
+  render() {
+    return (
+      <Router>
+        <div></div>
+      </Router>
+    );
+  }
+}
+class Index extends Component {
+  render() {
+    return (
+      <Router>
+        <div></div>
+      </Router>
+    );
+  }
+}
+class Search extends Component {
   render() {
     return (
       <Router>
