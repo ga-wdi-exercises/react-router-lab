54682ab9dbd00b320e0c49f01d0c9eb588e32357
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-05-30 11:22:44 -0400

    about part

--- src/Stock/Stock.js
+++ src/Stock/Stock.js
@@ -1,1 +1,25 @@
 import React, { Component } from 'react'
+
+class Stock extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      stock: this.props.location.state.selectedStock
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1> {this.state.stock.name} ({this.state.stock.symbol}) </h1>
+        <ul>
+          <li>Last Price: {this.state.stock.lastPrice}</li>
+          <li>Change: {this.state.stock.change}</li>
+          <li>High: {this.state.stock.high}</li>
+          <li>Low: {this.state.stock.low}</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Stock;
