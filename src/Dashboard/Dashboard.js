7e330d152f8c62f43f919b56bc80b18d29e1d442
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-05-30 11:08:51 -0400

    dashboard

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -2,7 +2,32 @@ import React, { Component } from 'react'
 import { Link } from "react-router-dom"
 
 class Dashboard extends Component {
+  constructor(props){
+    super(props)
+  }
 
+  render(){
+    let stocks = this.props.stocks.map((stock, i) => {
+      let pathname = `/stocks/${stocks.symbol}`
+        return <li className = "stocks_stock" key={i}>
+          {stock.name} (<Link to={{
+            pathname,
+            state: {selectedStock: stock}
+          }}>
+          {stock.symbol}
+          </Link>
+        )
+        </li>
+    })
+    return (
+      <div>
+        <h1> Stocks </h1>
+        <ul className="stock_list">
+          {stocks}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
