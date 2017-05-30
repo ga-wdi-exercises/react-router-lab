fca62e84c28c1313561ffb5d1f8b814c96234745
lgmusic24 <lgmusic24@gmail.com>
2017-05-30 18:44:50 -0400

    updated changes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,61 @@ import {
   Link,
   Redirect
 } from "react-router-dom"
+import stockData from '../data/stock-data'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div></div>
+        <div>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/search">Search</Link></li>
+        <hr/>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/search" component={Search}/>
+        </div>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+class Home extends Component {
+  render() {
+    console.log(stockData)
+    return (
+      <div>
+      <h2>Home</h2>
+      {stockData.map((stock, index) => (
+        <h3 key={index}><Link to={`/stocks/${stock.symbol}`}>{stock.name}</Link></h3>
+      ))}
+      </div>
+    )
+  }
+}
+
+const About = () => (
+  <div>
+  <h2>About</h2>
+  </div>
+)
+
+class Search extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Search</h2>
+        <form>
+        <input onChange={this.handleSearchInput} type='text' placeholder='Enter a stock symbol' />
+        <button type='submit'>submit</button>
+        </form>
+      </div>
+
+    )
+  }
+}
+
+
+  export default App;
