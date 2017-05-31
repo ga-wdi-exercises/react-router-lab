0efbe0572c32dce893c0edddf68ca66c1bf8d733
Paul Christian-Brown <pchristianbrown@gmail.com>
2017-05-31 01:07:21 -0400

    Added App and About js

--- src/App.js
+++ src/App.js
@@ -12,47 +12,36 @@ import About from './About'
 import Index from './Index'
 import Search from './Search'
 
-class Dashboard extends Component {
-  render() {
-    return (
-      <Router>
-        <div></div>
-      </Router>
-    );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      stocks: [
+        {name: "Verizon", symbol: "VZ", lastPrice: 46.20, volume: 3,709,618, high: 46.33, low: 45.33, open: 45.34},
+        {name: "Microsoft Corporation", symbol: "MSFT", lastPrice: 70.41, volume: 4,788,282, high: 70.41, low: 69.77, open: 69.79},
+        {name: "Nike.", symbol: "NKE", lastPrice: 52.92, volume: 2,130,859, high: 53.02, low: 52.64, open: 52.80},
+        {name: "Cisco", symbol: "CSCO", lastPrice: 31.68, volume: 4,832,461, high: 31.81, low: 31.39, open: 31.39},
+        {name: "Proctor & Gamble", symbol: "PG", lastPrice: 87.40, volume: 1,302,195, high: 87.60, low: 87.10, open: 97.25},
+        {name: "Boeing", symbol: "BA", lastPrice: 168.88, volume: 547,807, high: 187.42, low: 186.21, open: 186.76}
+      ]
+    }
   }
-}
-class Stock extends Component {
   render() {
     return (
       <Router>
-        <div></div>
-      </Router>
-    );
-  }
-}
-class About extends Component {
-  render() {
-    return (
-      <Router>
-        <div></div>
-      </Router>
-    );
-  }
-}
-class Index extends Component {
-  render() {
-    return (
-      <Router>
-        <div></div>
-      </Router>
-    );
-  }
-}
-class Search extends Component {
-  render() {
-    return (
-      <Router>
-        <div></div>
+        <div>
+          <div className="nav">
+            <div className="nav-item"><span className="nav-logo">iStocks</span></div>
+            <div className="nav-item"><Link to="/">Home</Link></div>
+            <div className="nav-item"><Link to="/about">About</Link></div>
+          </div>
+
+          <div className="main">
+            <Route exact path="/" render={() => <Dashboard stocks={this.state.stocks} />} />
+            <Route path="/about" component={About} />
+            <Route path="/stocks/:symbol" component={Stock} />
+          </div>
+        </div>
       </Router>
     );
   }
