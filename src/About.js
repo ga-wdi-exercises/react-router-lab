299033688f230aa1a861276e41c215923dc75f25
tedlayea <tedlayea@gMAIL.com>
2017-07-12 11:17:06 -0400

    link and roue done

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class About extends Component {
+  render(){
+    return(
+      <div>
+        <p> This is a stock tracking app.</p>
+        <p> This app  is bulit using React.</p>
+      </div>
+    )
+  }
+}
+
+export default About
---
+++ src/StockList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+class StockList extends Component{
+  render(){
+    return(
+      <div>
+      <p>Here is the stock names, please click on your interest to see detail information.</p>
+
+      </div>
+    )
+  }
+}
+
+export default StockList
