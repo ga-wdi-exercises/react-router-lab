da895ac3af32d68aff22b2609719b96909b79c16
Robert Lowe <RobLowe1992@Yahoo.com>
2017-07-12 23:24:44 -0400

    branch check

--- src/Stock.js
+++ src/Stock.js
@@ -3,12 +3,18 @@ import './Stock.css'
 import { Button } from 'react-bootstrap';
 
 export default class Stock extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            stock: this.props.location.stock.selection
+        }
+    }
     render() {
         return (
             <li className="Stock">
-                <a className="symbol">{this.props.showStock.symbol}</a>
+                <a className="symbol">{this.state.stock.symbol}</a>
                 <span> - </span>
-                <span>{this.props.showStock.name}</span>
+                <span>{this.state.stock.name}</span>
             </li>
         );
     }
