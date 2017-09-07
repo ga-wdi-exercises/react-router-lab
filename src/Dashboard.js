c6b014119933a61ea5d821f7c18d8f732eb28b4c
Perry Fustero <pjfust@gmail.com>
2017-09-07 11:56:37 -0400

    still working

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,20 +1,52 @@
 import React, { Component } from 'react'
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props)
+  }
+  
   render() {
-    var stockData = this.props.stocks.forEach((stock) => {
-      return (<li> stock.name </li>)
+    var stocks = this.props.stocks.map((stock, i) => {
+      return (
+        <li key ={i}>
+          {stock.name}
+        </li>)
     })
 
     return (
       <div>
         <h1>Welcome Home</h1>
         <ul>
-          {stockData}
+          {stocks}
         </ul>
       </div>
     )
   }
 }
 
-export default Dashboard
+// class Dashboard extends Component {
+//   constructor(props){
+//     super(props)
+//   }
+//   render() {
+//     let stocks = this.props.stocks.map((stock, i) => {
+//       let pathname = `/stocks/${stock.symbol}`
+//       return <li className="stocks-stock" key={i}>
+//          {stock.name} (<Link to={{
+//               pathname,
+//               state: {selectedStock: stock}
+//             }}>
+//             {stock.symbol}
+//           </Link>)
+//        </li>
+//     })
+//     return (
+//       <div className="stocks">
+//         <h2>Stocks</h2>
+//         <ul className="stocks-list">
+//           {stocks}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
