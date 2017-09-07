0295fad6234f5a9ee73ac1c977969620ad15ff83
Jacob Greenwald <jeg4t@virginia.edu>
2017-09-07 11:01:31 -0400

    more about

--- src/About.js
+++ src/About.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 
 class About extends Component {
-  constructor (props) {
-    super (props)
+  constructor(props) {
+    super(props)
   }
   render () {
     return (
