e91ae07af9041280333f29de9f5add44c4ae2121
Charles Cox <cox.charles.w@gmail.com>
2017-09-07 10:39:08 -0400

    nav bar show home and about

--- src/App.js
+++ src/App.js
@@ -8,10 +8,59 @@ import {
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+
   render() {
     return (
       <Router>
-        <div></div>
+        <div>
+          <nav>
+            <h1>WDI Stock Tracker</h1>
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+          </nav>
+          <main>
+            <Switch>
+            <Route
+              path="/home"
+              render={(props) => {
+                return (
+                  <Search
+                  {...props}
+                  translation={ this.state.translation }
+                  setTranslation={ (data, language) => this.setTranslation(data, language) }
+                  />
+                )
+              }}
+            />
+            <Route
+              path="/about"
+              render={(props) => {
+                return (
+                  <Results
+                    {...props}
+                    translation={ this.state.translation }
+                    language={this.state.langauge}
+                  />
+                )
+              }}
+            />
+            <Route
+              path="/*"
+              render={() => {
+                return (
+                  <Redirect to="/home" />
+                )
+              }}
+            />
+            </Switch>
+          </main>
+        </div>
       </Router>
     );
   }
