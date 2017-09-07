a7455c1dbc9aae455d33e9176cb33e69f856f4b6
Jacob Greenwald <jeg4t@virginia.edu>
2017-09-07 13:32:51 -0400

    add jquery and clean ajax response

--- src/Stock.js
+++ src/Stock.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
+import $ from 'jquery'
 
 class Stock extends Component {
-  constructor(props){
+  constructor (props) {
     super(props)
     this.state = {
-      stock: this.props.location.state.selectedStock
+      stock: this.props.location.state.selectedStock,
+      apiStock: {}
     }
-
   }
-  render() {
-    console.log(this.state.stock);
+  componentDidMount () {
+    const domain = `https://www.alphavantage.co/`
+    let current = this.state.stock.symbol
+    const apiEndpoint = `query?function=TIME_SERIES_INTRADAY&symbol=${current}&interval=1min&apikey=`
+    let apiKey = '1O39HQEQXJPVJY17'
+    let url = `${domain}${apiEndpoint}${apiKey}`
+
+    $.ajax({
+      url,
+      method: 'GET',
+      dataType: 'json'
+    }).then((response) => {
+      let stockSymbol = Object.values(response)[0]["2. Symbol"]
+      let prices = Object.values(response)[1]
+      let latest = Object.values(prices)[0]
+      console.log(stockSymbol)
+      console.log(prices[1])
+      console.log(latest)
+      let newStockObject = {
+        symbol: stockSymbol,
+        high: latest["2. high"],
+        low: latest["3. low"],
+        change: latest["4. close"] - latest["1. open"]
+      }
+      console.log(newStockObject);
+
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+  render () {
     return (
       <div>
         <h2>Stock {this.state.stock.name} (Stock {this.state.stock.symbol})</h2>
@@ -20,8 +50,8 @@ class Stock extends Component {
           <li>Low: {this.state.stock.low}</li>
         </ul>
       </div>
-    );
+    )
   }
 }
 
-export default Stock;
+export default Stock
