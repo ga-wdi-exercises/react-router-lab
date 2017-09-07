b613297fe92165e48308c6038eff799158c86f30
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-09-07 12:32:30 -0400

    working on stock show page

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Redirect
-} from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 
 class Dashboard extends Component {
---
+++ src/Stock.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Stock extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      stock: this.props.location.state.selectedStock
+    }
+  }
+
+  render () {
+    return (
+      <h2>stock detail</h2>
+    )
+  }
+}
+export default Stock
