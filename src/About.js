57bf64407ee05521c1bd55bac93f636427e4a622
Mitche Guevara <mitche.guevara@gmail.com>
2017-09-10 01:30:57 -0400

    push

--- src/About.js
+++ src/About.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 class About extends Component {
+  constructor(props){
+    super(props)
+  }
   render () {
 
     return (
--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import About from './About'
+// import data from '../data/stock-data.json'
 import {
   BrowserRouter as Router,
   Route,
   Link,
-  Redirect
-} from "react-router-dom"
-import './App.css';
-import Home from './Home';
-import About from './About'
+  Switch
+} from 'react-router-dom'
+import './App.css'
 
 class App extends Component {
-  render() {
+
+  render () {
     return (
       <Router>
         <div>
-          <nav>
-            <Link to="/Home">Home</Link>
-            <Link to="/About">About</Link>
-          </nav>
+          <div className='nav'>
+            <div className='nav-item'><Link to='/'>Home</Link></div>
+            <div className='nav-item'><Link to='/about'>About</Link></div>
+          </div>
           <main>
-            <Route path="/Home"
-            render={() => {
-              return <Home  />}}/>
-            <Route path="/About"
-            render={() => {
-              return <About />
-            }}/>
+            <Switch>
+              <Route
+                path='/about'
+                component={About}
+              />
+            </Switch>
           </main>
+
         </div>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App
